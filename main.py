1e876c4dbfcc3badb2b26613f84cffbe51b55d65
namang <namangupta9@gmail.com>
2018-01-13 00:47:55 -0500

    added css for dash

--- main.py
+++ main.py
@@ -6,29 +6,13 @@ import matchdata_exporter
 # MAIN PROGRAM DRIVER
 def main(season_in) :
 
-    # Declare Teams
+    # Load Teams
     # [Official Name, Shorthand (Taken from Google's EPL Tables)]
     teams = []
-    teams.append(Club("afc_bournemouth", "Bournemouth", ["Association Football Club Bournemouth", "Bournemouth"]))
-    teams.append(Club("arsenal", "Arsenal", ["Arsenal Football Club", "Arsenal"]))
-    teams.append(Club("aston_villa", "Aston Villa", ["Aston Villa Football Club", "Aston Villa"]))
-    teams.append(Club("chelsea", "Chelsea", ["Chelsea Football Club", "Chelsea"]))
-    teams.append(Club("crystal_palace", "Crystal Palace", ["Crystal Palace Football Club", "Crystal Palace"]))
-    teams.append(Club("everton", "Everton", ["Everton Football Club", "Everton"]))
-    teams.append(Club("leicester_city", "Leicester", ["Leicester City Football Club", "Leicester City"]))
-    teams.append(Club("liverpool", "Liverpool", ["Liverpool Football Club", "Liverpool"]))
-    teams.append(Club("manchester_city", "Manchester City", ["Manchester City Football Club", "Man City"]))
-    teams.append(Club("manchester_united", "Manchester United", ["Manchester United Football Clubs", "Man United"]))
-    teams.append(Club("newcastle_united", "Newcastle United", ["Newcastle United Football Club", "Newcastle"]))
-    teams.append(Club("norwich_city", "Norwich", ["Norwich City Football Club", "Norwich City"]))
-    teams.append(Club("southampton", "Southampton", ["Southampton Football Club", "Southampton"]))
-    teams.append(Club("stoke_city", "Stoke", ["Stoke City Football Club", "Stoke City"]))
-    teams.append(Club("sunderland", "Sunderland", ["Sunderland Football Club", "Sunderland"]))
-    teams.append(Club("swansea_city", "Swansea", ["Swansea City Football Club", "Swansea City"]))
-    teams.append(Club("tottenham", "Tottenham", ["Tottenham Hotspur Football Club", "Tottenham"]))
-    teams.append(Club("watford", "Watford", ["Watford Football Club", "Watford"]))
-    teams.append(Club("west_brom", "West Bromwich Albion", ["West Bromwich Albion Football Club", "West Brom"]))
-    teams.append(Club("west_ham", "West Ham", ["West Ham Football Club", "West Ham"]))
+    with open("teams.txt", 'r') as file:
+        for c in file:
+            names = c.split(', ')
+            teams.append(Club(names[0], names[1], names[2:]))
 
     # Scrape Match Data
     trends_scraper.scrape_match_data(season_in, teams)
--- viz.py
+++ viz.py
@@ -1,16 +1,34 @@
-"""handles the creation of an interactive data visualization page for thesis"""
+"""handles creation of an interactive data visualization page for thesis."""
 
 # Naman Gupta
 # namangupta.co
 # 2018
 
+import pandas as pd
+
 # coding: utf-8
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 
 # start
 app = dash.Dash()
+
+
+def get_club_dropdowns():
+    """Return a list of dicts of dropdown options, one for each club."""
+    dropdowns = []
+
+    # iterate through file, create new dropdown option for each club
+    with open("teams.txt", 'r') as file:
+        for c in file:
+            names = c.split(", ")
+            dropdowns.append({'label': names[1],
+                              'value': names[0]})
+
+    return dropdowns
+
 
 # css
 app.css.append_css({
@@ -19,13 +37,31 @@ app.css.append_css({
 
 # generate html layout
 app.layout = html.Div(children=[
-    html.H1(children='Senior Thesis - Visualization'),
 
+    # heading
+    html.H1(children='Senior Thesis: EPL Analytics'),
+    html.Div(children='Naman Gupta, Eric Schwartz', id="authors"),
     html.Div(children='''
-        EPL Analytics: Exploring how different match events affect
-        normalized Google search activity levels.
-    '''),
+        How might various match events in the English Premier League affect
+        normalized Google search activity?''', id="description"),
 
+    # dropdown to select club
+    html.Div([
+        dcc.Dropdown(
+            id='club-dropdown',
+            options=get_club_dropdowns(),
+            value='Select Club'
+        ),
+    ], style={'width': '500'}),
+
+    # another dropdown to select match from club's fixture list
+    html.Div([
+        dcc.Dropdown(
+            id='match-dropdown',
+            ),
+    ], style={'width': '500'}),
+
+    # graph to display match chart
     dcc.Graph(
         id='example-graph',
         figure={
@@ -46,6 +82,39 @@ app.layout = html.Div(children=[
         )
     ]
 )
+
+
+@app.callback(Output('match-dropdown', 'options'),
+              [Input('club-dropdown', 'value')])
+def get_match_dropdowns(input_value):
+    """Return a list of dicts of dropdown options, one for each match."""
+    dropdowns = []
+
+    # load club's match information
+    info_filepath = "Match Information/" + input_value + "_matches_2016.csv"
+    df = pd.read_csv(info_filepath)
+
+    # iterate through dates to create dropdown options
+    dates = df["date"]
+    for date in dates:
+        dropdowns.append({'label': date,
+                          'value': date})
+
+    return dropdowns
+
+
+@app.callback(Output('match-chart', 'figure'),
+              [Input('club-dropdown', 'value'),
+               Input('match-dropdown', 'value')])
+def get_match_chart(club_dropdown, match_dropdown):
+    """Plot a figure based on the selected club & match."""
+
+    # load search volume data frame & select relevant area TODO
+
+    # load match information data frame & select relevant area TODO
+
+    # using search volume & match information data, plot! TODO
+
 
 # execution
 if __name__ == '__main__':
