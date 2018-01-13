a2b813bf3a10d105c66a21937fad3ce62eec8233
namang <namangupta9@gmail.com>
2018-01-13 12:40:11 -0500

    working on plotting

--- viz.py
+++ viz.py
@@ -5,30 +5,21 @@
 # 2018
 
 import pandas as pd
+from ast import literal_eval
+from viz_helpers import get_club_dropdowns
+from viz_helpers import get_relevant_data
+from viz_helpers import get_match_event_lines
 
 # coding: utf-8
 import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly
+import plotly.graph_objs as go
 
 # start
 app = dash.Dash()
-
-
-def get_club_dropdowns():
-    """Return a list of dicts of dropdown options, one for each club."""
-    dropdowns = []
-
-    # iterate through file, create new dropdown option for each club
-    with open("teams.txt", 'r') as file:
-        for c in file:
-            names = c.split(", ")
-            dropdowns.append({'label': names[1],
-                              'value': names[0]})
-
-    return dropdowns
-
 
 # css; use rawgit.com to serve raw files directly from github repo
 # dash only allows the usage of externally-hosted css & javascript for now
@@ -50,6 +41,7 @@ app.layout = html.Div(children=[
         dcc.Dropdown(
             id='club-dropdown',
             options=get_club_dropdowns(),
+            value="Chelsea"
         ),
     ], style={'width': '500'}),
 
@@ -61,8 +53,11 @@ app.layout = html.Div(children=[
     ], style={'width': '500'}),
 
     # graph to display match chart
-    dcc.Graph()
-    ]
+    html.Div([
+        dcc.Graph(
+            id='match-chart',
+            ),
+    ], style={'width': '500'})]
 )
 
 
@@ -85,17 +80,49 @@ def get_match_dropdowns(input_value):
     return dropdowns
 
 
-# @app.callback(Output('match-chart', 'figure'),
-#               [Input('club-dropdown', 'value'),
-#                Input('match-dropdown', 'value')])
-# def get_match_chart(club_dropdown, match_dropdown):
-#     """Plot a figure based on the selected club & match."""
-#
-#     # load search volume data frame & select relevant area TODO
-#
-#     # load match information data frame & select relevant area TODO
-#
-#     # using search volume & match information data, plot! TODO
+@app.callback(Output('match-chart', 'figure'),
+              [Input('club-dropdown', 'value'),
+               Input('match-dropdown', 'value')])
+def get_match_chart(club_dropdown, match_dropdown):
+    """Plot a figure based on the selected club & match."""
+    # load search volume & match information data frames
+    search_file = club_dropdown + "_match_2016.csv"
+    info_file = club_dropdown + "_matches_2016.csv"
+    search_df = pd.read_csv("Matchday Volumes/" + search_file)
+    info_df = pd.read_csv("Match Information/" + info_file)
+
+    # some preliminary data formatting
+    search_df['time'] = search_df.apply(lambda row: row.date[-8:], axis=1)
+    search_df['date'] = search_df.apply(lambda row: row.date[:-9], axis=1)
+    search_df = search_df[search_df.time != "00:00:00"]
+
+    # format match event timestamps
+    info_df["away_goals"] = info_df["away_goals"].apply(
+                            lambda x: literal_eval(x))
+    info_df["home_goals"] = info_df["home_goals"].apply(
+                            lambda x: literal_eval(x))
+    info_df["disallowed_goals"] = info_df["disallowed_goals"].apply(
+                                  lambda x: literal_eval(x))
+    info_df["home_reds"] = info_df["home_reds"].apply(
+                           lambda x: literal_eval(x))
+    info_df["away_reds"] = info_df["away_reds"].apply(
+                           lambda x: literal_eval(x))
+    info_df["home_yellows"] = info_df["home_yellows"].apply(
+                              lambda x: literal_eval(x))
+    info_df["away_yellows"] = info_df["away_yellows"].apply(
+                              lambda x: literal_eval(x))
+
+    # using search volume & match information data, plot!
+    layout = go.Layout(
+        xaxis=dict(showgrid=True, zeroline=False, showline=True),
+        yaxis=dict(showgrid=True, zeroline=False, showline=True),
+        shapes=get_match_event_lines(info_df, match_dropdown)
+    )
+
+    fig = plotly.graph_objs.Figure(data=get_relevant_data(search_df,
+                                                          match_dropdown),
+                                   layout=layout)
+    plotly.offline.iplot(fig)
 
 
 # execution
---
+++ viz_helpers.py
@@ -0,0 +1,155 @@
+"""helper functions for the creation of interactive viz website."""
+
+# Naman Gupta
+# namangupta.co
+# 2018
+
+import plotly.graph_objs as go
+
+
+def get_club_dropdowns():
+    """Return a list of dicts of dropdown options, one for each club."""
+    dropdowns = []
+
+    # iterate through file, create new dropdown option for each club
+    with open("teams.txt", 'r') as file:
+        for c in file:
+            names = c.split(", ")
+            dropdowns.append({'label': names[1],
+                              'value': names[0]})
+
+    return dropdowns
+
+
+def get_relevant_data(search_vol_df, match_dropdown):
+    """Return a list of scatter plot objects to visualize search volumes."""
+    data = []
+
+    match_searches = search_vol_df.loc[search_vol_df["date"] == match_dropdown]
+
+    data.append(go.Scatter(
+                    x=match_searches.time,
+                    y=match_searches.iloc[:, 1],
+                    name="Official Name, Unscaled",
+                    line=dict(color='#c9c7c7'),
+                    opacity=0.8))
+
+    data.append(go.Scatter(
+                    x=match_searches.time,
+                    y=match_searches.iloc[:, 2],
+                    name="Shorthand",
+                    line=dict(color='#7F7F7F'),
+                    opacity=0.8))
+
+    return data
+
+
+def get_match_event_lines(match_info_df, m_drop):
+    """Return a list of line objects to mark individual match events."""
+    lines = []
+
+    # relevant_match_info
+    relevant_match_info = match_info_df.iloc[match_info_df["date"] == m_drop]
+
+    # kickoff time & estimated end time
+    # end time estimated as:
+    # (kickoff + 90' + 6' est. total stoppage time + 15' Halftime)
+    #  / 8 (graph scale)
+    kt = relevant_match_info["kickoff_time"]
+    kt_float = float((int(kt[0:2]) * 60) + (int(kt[3:5])) - 64) / 8
+    lines.append({
+            'type': 'rect',
+            'x0': kt_float,
+            'y0': 0,
+            'x1': kt_float + 13.875,
+            'y1': 100,
+            'fillcolor': 'rgb(55, 128, 191)',
+            'opacity': 0.1,
+            'line': {'width': 0}
+            })
+
+    # home goals
+    for goal in relevant_match_info["home_goals"]:
+        hg_float = float((int(goal[0:2]) * 60) + (int(goal[3:5])) - 64) / 8
+        lines.append({
+                'type': 'line',
+                'x0': hg_float,
+                'y0': 0,
+                'x1': hg_float,
+                'y1': 100,
+                'line': {
+                    'color': '#50f441',
+                    'width': 2}
+                })
+
+    # away goals
+    for goal in relevant_match_info["away_goals"]:
+        ag_float = float((int(goal[0:2]) * 60) + (int(goal[3:5])) - 64) / 8
+        lines.append({
+                'type': 'line',
+                'x0': ag_float,
+                'y0': 0,
+                'x1': ag_float,
+                'y1': 100,
+                'line': {
+                    'color': '#4158f4',
+                    'width': 2}
+                })
+
+    # home red cards
+    for card in relevant_match_info["home_reds"]:
+        hr_float = float((int(card[0:2]) * 60) + (int(card[3:5])) - 64) / 8
+        lines.append({
+                'type': 'line',
+                'x0': hr_float,
+                'y0': 0,
+                'x1': hr_float,
+                'y1': 100,
+                'line': {
+                    'color': 'red',
+                    'width': 1}
+                })
+
+    # home yellow cards
+    for card in relevant_match_info["home_yellows"]:
+        hy_float = float((int(card[0:2]) * 60) + (int(card[3:5])) - 64) / 8
+        lines.append({
+                'type': 'line',
+                'x0': hy_float,
+                'y0': 0,
+                'x1': hy_float,
+                'y1': 100,
+                'line': {
+                    'color': 'yellow',
+                    'width': 1}
+                })
+
+    # away red cards
+    for card in relevant_match_info["away_reds"]:
+        ar_float = float((int(card[0:2]) * 60) + (int(card[3:5])) - 64) / 8
+        lines.append({
+                'type': 'line',
+                'x0': ar_float,
+                'y0': 0,
+                'x1': ar_float,
+                'y1': 100,
+                'line': {
+                    'color': 'red',
+                    'width': 1}
+                })
+
+    # away yellow cards
+    for card in relevant_match_info["away_yellows"]:
+        ay_float = float((int(card[0:2]) * 60) + (int(card[3:5])) - 64) / 8
+        lines.append({
+                'type': 'line',
+                'x0': ay_float,
+                'y0': 0,
+                'x1': ay_float,
+                'y1': 100,
+                'line': {
+                    'color': 'yellow',
+                    'width': 1}
+                })
+
+    return lines
