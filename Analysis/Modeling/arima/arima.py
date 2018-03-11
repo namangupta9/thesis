0db52cc6f82c271df74a87b929bb112371125eee
namang <namangupta9@gmail.com>
2018-03-11 16:23:45 -0400

    initial arima modeling

--- Analysis/Modeling/arima/arima.py
+++ Analysis/Modeling/arima/arima.py
@@ -1,59 +1,89 @@
 """ARIMA Modeling (Time Series Analysis)."""
 
-# What is ARMA, and ARIMA? Why ARIMA > Multiple Linear Regression?
-# https://machinelearningmastery.com/arima-for-time-series-forecasting-with-python/
-
-# An ARIMA model is a class of statistical models for analyzing and forecasting
-# time series data. It explicitly caters to a suite of standard structures in
-# time series data, and as such provides a simple yet powerful method for
-# making skillful time series forecasts.
-
-# ARIMA is an acronym that stands for AutoRegressive Integrated Moving Average.
-# It is a generalization of the simpler AutoRegressive Moving Average and adds
-# the notion of integration.
-
-# This acronym is descriptive, capturing the key aspects of the model itself:
-
-# AR: Autoregression.
-#     A model that uses the dependent relationship between an observation
-#     and some number of lagged observations.
-
-# I: Integrated.
-#    The use of differencing of raw observations (e.g. subtracting an
-#    observation from an observation at the previous time step) in order to
-#    make the time series stationary.
-
-# MA: Moving Average.
-#     A model that uses the dependency between an observation and a residual
-#     error from a moving average model applied to lagged observations.
-
-# Each component is explicitly specified in the model as a parameter.
-
-# A standard notation is used of ARIMA(p,d,q) where the parameters are
-# substituted with integer values to quickly indicate the specific ARIMA model
-# being used.
-
-# The parameters of the ARIMA model are defined as follows:
-
-# p: # of lag observations included in the model (lag order)
-# d: # of times that the raw observations are differenced (degree of differencing)
-# q: The size of the moving average window, (order of moving average)
-
-# A linear regression model is constructed including the specified number
-# and type of terms, and the data is prepared by a degree of differencing in
-# order to make it stationary, i.e. to remove trend and seasonal structures
-# that negatively affect the regression model.
-
-# A value of 0 can be used for a parameter, which indicates to not use that
-# element of the model. This way, the ARIMA model can be configured to perform
-# the function of an ARMA model, and even a simple AR, I, or MA model.
-
-# Adopting an ARIMA model for a time series assumes that the underlying process
-# that generated the observations is an ARIMA process. This may seem obvious,
-# but helps to motivate the need to confirm the assumptions of the model in the
-# raw observations and in the residual errors of forecasts from the model.
-
 import pandas as pd
 from dplython import *
-import statsmodels.api as sm
+from statsmodels.tsa.arima_model import ARIMA
 # http://www.statsmodels.org/dev/generated/statsmodels.tsa.arima_model.ARIMA.html
+
+# read initial data
+longform_df = DplyFrame(pd.read_csv("../../LongForm/longform.csv",
+                        dtype={'shorthand_search_vol': float}))
+
+# process data
+longform_df["date"] = longform_df.match_id.apply(lambda x: "20" + x.split("20")[-1])
+longform_df['date_time'] = longform_df['date'].astype(str) + " " + longform_df['time'].astype(str)
+longform_df['date_time'] = pd.to_datetime(longform_df['date_time'], errors="coerce", infer_datetime_format=True)
+
+# fit several ARIMA(2, 0, 2) models
+# TODO: why did Schwartz use 2, 0, 2 as model parameters?
+# TODO: why no Integration? why are we using ARMA?
+
+# ARMA MODEL #1: USING BASIC MATCH EVENTS
+x_mat = longform_df >> select(longform_df.home_goal,
+                              longform_df.away_goal,
+                              longform_df.home_yellow,
+                              longform_df.away_yellow,
+                              longform_df.home_red,
+                              longform_df.away_red,
+                              longform_df.stage_1_ind,
+                              longform_df.stage_2_ind,
+                              longform_df.stage_3_ind,
+                              longform_df.stage_4_ind,
+                              longform_df.competitive_idx)
+
+model = ARIMA(endog=longform_df.shorthand_search_vol,
+              exog=x_mat,
+              dates=longform_df.date_time,
+              order=(2, 0, 2))
+
+model_fit = model.fit(disp=0)   # disp=0 turns off debug information
+with open('model1.txt', 'w') as f:
+    # print summary
+    print >> f, model_fit.summary()
+
+
+# ARMA MODEL #2: USING ALL VARS
+x_mat = longform_df >> select(longform_df.match_wk,
+                              longform_df.home_goal,
+                              longform_df.away_goal,
+                              longform_df.home_yellow,
+                              longform_df.away_yellow,
+                              longform_df.home_red,
+                              longform_df.away_red,
+                              longform_df.stage_1_ind,
+                              longform_df.stage_2_ind,
+                              longform_df.stage_3_ind,
+                              longform_df.stage_4_ind,
+                              longform_df.cum_total_goals,
+                              longform_df.cum_goal_diff,
+                              longform_df.man_down,
+                              longform_df.upset,
+                              longform_df.competitive_idx)
+
+model = ARIMA(endog=longform_df.shorthand_search_vol,
+              exog=x_mat,
+              dates=longform_df.date_time,
+              order=(2, 0, 2))
+
+model_fit = model.fit(disp=0)   # disp=0 turns off debug information
+with open('model2.txt', 'w') as f:
+    # print summary
+    print >> f, model_fit.summary()
+
+# LET'S ONLY FOCUS ON STAGE 2
+stage_2_df = longform_df >> sift(X.stage_2_ind == 1)
+
+# LET'S START BUILDING IN SOME INTERACTIONS..
+# multiply dummy variable values together
+# for example, what's the effect of a home or away goal when an upset's in progress?
+# this new column will only be 1 when both home/away goal are 1, and upset is 1 (1*1=1)
+# moving away from considering variables strictly in isolation, probably more accurate w/ interactions
+
+stage_2_df['home_goal_AND_upset'] = stage_2_df['home_goal'].astype(int) * stage_2_df['upset'].astype(int)
+stage_2_df['away_goal_AND_upset'] = stage_2_df['away_goal'].astype(int) * stage_2_df['upset'].astype(int)
+stage_2_df['man_down_AND_upset'] = stage_2_df['man_down'].astype(int) * stage_2_df['upset'].astype(int)
+
+# other ideas for interactions
+# goal diff of 0 and a goal?
+# goal diff of 0 and cum goal diff > 5? really offensive but tight game
+# goal diff of 0 and cum goal diff < 5? really defensive but tight game
--- Analysis/Modeling/plots/plotting.py
+++ Analysis/Modeling/plots/plotting.py
@@ -7,13 +7,14 @@ from ggplot import *
 from dplython import *
 
 # initial data processing
-longform_df = DplyFrame(pd.read_csv("../LongForm/longform.csv", dtype={'shorthand_search_vol': int}))
+longform_df = DplyFrame(pd.read_csv("../../LongForm/longform.csv", dtype={'shorthand_search_vol': int}))
 longform_df["date"] = longform_df.match_id.apply(lambda x: "20" + x.split("20")[-1])
 longform_df['date_time'] = longform_df['date'].astype(str) + " " + longform_df['time'].astype(str)
 longform_df['date_time'] = pd.to_datetime(longform_df['date_time'], errors="coerce", infer_datetime_format=True)
 
 # filtering with dplyr
 longform_df >> sift(X.match_wk == 0)
+
 
 # plot matchweek 1
 p = ggplot(aes(x='date_time',
@@ -25,6 +26,35 @@ p = ggplot(aes(x='date_time',
 p += geom_line()
 p += scale_x_date(labels=date_format("%H:%M:%S"), date_breaks="1 hour")
 p += facet_grid('date', scales='free_x')
-p += labs(x="Time", y="Search Volume (Scaled to 100)")
-p += ggtitle("Matchweek 1")
+p += labs(x="Time (GMT)", y="Search Volume (Scaled to 100)")
+p += ggtitle("EPL Matchweek 1, 2015/2016")
 p.save('matchweek1.png', width=25, height=10)
+
+
+# plotting an individual match (w/ match events!)
+p = ggplot(aes(x='date_time',
+               y='shorthand_search_vol',
+               group="match_id",
+               color="match_id"),
+           data=longform_df >> sift(X.match_id == "chelsea2015-08-16"))
+
+p += geom_line()
+
+p += labs(x="Time (GMT)", y="Search Volume (Scaled to 100)")
+p += ggtitle("Manchester City (H) vs. Chelsea (A), August 8th 2016, Etihad Stadium")
+
+# add in shaded rectangle to indicate actual match TODO
+# rect_df = longform_df >> sift(X.stage_2_ind == 1) >> \
+#                          sift(X.match_id == "chelsea2015-08-16")
+#
+# p += geom_rect(df, aes(xmin="start", xmax="end", ymin=0, ymax=100))
+
+# add in lines for home & away goals
+# filtered_df = longform_df >> sift(X.home_goal == 1) >> \
+#                              sift(X.match_id == "chelsea2015-08-16")
+#
+# for index, row in filtered_df.iterrows():
+#     p += geom_vline(xintercept=[row["date_time"]], color='red')
+
+p += scale_x_date(labels=date_format("%H:%M:%S"), date_breaks="30 minutes")
+p.save('chelsea2015-08-16.png', width=25, height=10)
