ca426785a74e434a37c680e04efdd3ec8143fa54
namang <namangupta9@gmail.com>
2018-03-11 18:55:25 -0400

    more arima modeling

--- Analysis/Modeling/arima/arima.py
+++ Analysis/Modeling/arima/arima.py
@@ -1,5 +1,6 @@
 """ARIMA Modeling (Time Series Analysis)."""
 
+
 import pandas as pd
 from dplython import *
 from statsmodels.tsa.arima_model import ARIMA
@@ -16,9 +17,8 @@ longform_df['date_time'] = pd.to_datetime(longform_df['date_time'], errors="coer
 
 # fit several ARIMA(2, 0, 2) models
 # TODO: why did Schwartz use 2, 0, 2 as model parameters?
-# TODO: why no Integration? why are we using ARMA?
 
-# ARMA MODEL #1: USING BASIC MATCH EVENTS
+# ARIMA MODEL #1: USING BASIC MATCH EVENTS
 x_mat = longform_df >> select(longform_df.home_goal,
                               longform_df.away_goal,
                               longform_df.home_yellow,
@@ -42,7 +42,7 @@ with open('model1.txt', 'w') as f:
     print >> f, model_fit.summary()
 
 
-# ARMA MODEL #2: USING ALL VARS
+# ARIMA MODEL #2: USING ALL VARS
 x_mat = longform_df >> select(longform_df.match_wk,
                               longform_df.home_goal,
                               longform_df.away_goal,
@@ -70,20 +70,153 @@ with open('model2.txt', 'w') as f:
     # print summary
     print >> f, model_fit.summary()
 
-# LET'S ONLY FOCUS ON STAGE 2
-stage_2_df = longform_df >> sift(X.stage_2_ind == 1)
 
-# LET'S START BUILDING IN SOME INTERACTIONS..
-# multiply dummy variable values together
-# for example, what's the effect of a home or away goal when an upset's in progress?
+# ARIMA MODEL #3: STAGE 2, MATCH EVENTS ONLY
+# LET'S ONLY FOCUS ON STAGE 2...makes more sense to consider match events in the context of the match itself,
+#                               and not the buildup or post-match reaction time...
+stage_2_df = longform_df >> sift(X.stage_2_ind == 1)
+stage_2_df = stage_2_df.reset_index(drop=True)
+
+# new, more thoughtful ARIMA model parameters
+# d = 1 ("first difference"); let's predict the delta b/w volumes at consecutive intervals
+# aka, "stationarizing" the time series
+# q = 1 (a series displays moving average behavior if it apparently undergoes random
+#        "shocks" whose effects are felt in 2+ consecutive periods. )
+# TODO, diff b/w q = 1 & q = 2?
+
+x_mat = stage_2_df >> select(stage_2_df.home_goal,
+                             stage_2_df.away_goal,
+                             stage_2_df.home_yellow,
+                             stage_2_df.away_yellow,
+                             stage_2_df.home_red,
+                             stage_2_df.away_red,
+                             stage_2_df.competitive_idx)
+
+model = ARIMA(endog=stage_2_df.shorthand_search_vol,
+              exog=x_mat,
+              dates=stage_2_df.date_time,
+              order=(0, 1, 1))
+
+model_fit = model.fit(disp=0)   # disp=0 turns off debug information
+with open('model3.txt', 'w') as f:
+    # print summary
+    print >> f, model_fit.summary()
+
+
+# ARIMA MODEL #4: STAGE 2, ALL VARS
+x_mat = stage_2_df >> select(stage_2_df.match_wk,
+                             stage_2_df.home_goal,
+                             stage_2_df.away_goal,
+                             stage_2_df.home_yellow,
+                             stage_2_df.away_yellow,
+                             stage_2_df.home_red,
+                             stage_2_df.away_red,
+                             stage_2_df.cum_total_goals,
+                             stage_2_df.cum_goal_diff,
+                             stage_2_df.man_down,
+                             stage_2_df.upset,
+                             stage_2_df.competitive_idx)
+
+model = ARIMA(endog=stage_2_df.shorthand_search_vol,
+              exog=x_mat,
+              dates=stage_2_df.date_time,
+              order=(0, 1, 1))
+
+model_fit = model.fit(disp=0)   # disp=0 turns off debug information
+with open('model4.txt', 'w') as f:
+    # print summary
+    print >> f, model_fit.summary()
+
+
+# ARIMA MODEL #5: STAGE 2, STATISTICAL INTERACTIONS
+
+# we're really making two types of predictions here...
+# predicting the impact (spikes) of individual match events (under certain conditions)
+# and we're predicting "raw" scaled levels under certain conditions...
+# -- like, we're not saying anything about the bump, but when there's a deadlock and upset,
+#    we're seeing generally higher search activity levels!
+# this later type of prediction should require a different model, right?
+# we wouldn't need to first difference anymore, because we don't care about the "bump"
+
+# TODO am i getting multiple collinearity problems here? idk what that fully means.
+# interactions: what's the effect of a home or away goal when an upset's in progress?
 # this new column will only be 1 when both home/away goal are 1, and upset is 1 (1*1=1)
 # moving away from considering variables strictly in isolation, probably more accurate w/ interactions
-
 stage_2_df['home_goal_AND_upset'] = stage_2_df['home_goal'].astype(int) * stage_2_df['upset'].astype(int)
 stage_2_df['away_goal_AND_upset'] = stage_2_df['away_goal'].astype(int) * stage_2_df['upset'].astype(int)
+stage_2_df['home_goal_AND_man_down'] = stage_2_df['home_goal'].astype(int) * stage_2_df['man_down'].astype(int)
+stage_2_df['away_goal_AND_man_down'] = stage_2_df['away_goal'].astype(int) * stage_2_df['man_down'].astype(int)
 stage_2_df['man_down_AND_upset'] = stage_2_df['man_down'].astype(int) * stage_2_df['upset'].astype(int)
 
-# other ideas for interactions
-# goal diff of 0 and a goal?
-# goal diff of 0 and cum goal diff > 5? really offensive but tight game
-# goal diff of 0 and cum goal diff < 5? really defensive but tight game
+# home / away goals when cum_goal_diff is 0? ("breaking deadlock")
+stage_2_df["deadlock"] = (stage_2_df["cum_goal_diff"].astype(int) == 0).astype(int)
+stage_2_df['home_goal_AND_deadlock'] = stage_2_df['home_goal'].astype(int) * stage_2_df['deadlock'].astype(int)
+stage_2_df['away_goal_AND_deadlock'] = stage_2_df['away_goal'].astype(int) * stage_2_df['deadlock'].astype(int)
+
+# other ideas
+# when goal_diff of 0 and cum_total_goals > 2? really offensive but tight game
+# when goal_diff of 0 and cum_total_goals < 2? really offensive but tight game
+
+x_mat = stage_2_df >> select(stage_2_df.match_wk,
+                             stage_2_df.home_goal,
+                             stage_2_df.away_goal,
+                             stage_2_df.home_yellow,
+                             stage_2_df.away_yellow,
+                             stage_2_df.home_red,
+                             stage_2_df.away_red,
+                             stage_2_df.home_goal_AND_upset,
+                             stage_2_df.away_goal_AND_upset,
+                             stage_2_df.home_goal_AND_man_down,
+                             stage_2_df.away_goal_AND_man_down,
+                             stage_2_df.man_down_AND_upset,
+                             stage_2_df.home_goal_AND_deadlock,
+                             stage_2_df.away_goal_AND_deadlock,
+                             stage_2_df.competitive_idx)
+
+model = ARIMA(endog=stage_2_df.shorthand_search_vol,
+              exog=x_mat,
+              dates=stage_2_df.date_time,
+              order=(0, 1, 1))
+
+model_fit = model.fit(disp=0)   # disp=0 turns off debug information
+with open('model4.txt', 'w') as f:
+    # print summary
+    print >> f, model_fit.summary()
+
+# ARIMA MODEL #5: STAGE 2, STRICTLY STATISTICAL INTERACTIONS
+x_mat = stage_2_df >> select(stage_2_df.match_wk,
+                             stage_2_df.home_goal_AND_upset,
+                             stage_2_df.away_goal_AND_upset,
+                             stage_2_df.home_goal_AND_man_down,
+                             stage_2_df.away_goal_AND_man_down,
+                             stage_2_df.man_down_AND_upset,
+                             stage_2_df.home_goal_AND_deadlock,
+                             stage_2_df.away_goal_AND_deadlock,
+                             stage_2_df.competitive_idx)
+
+model = ARIMA(endog=stage_2_df.shorthand_search_vol,
+              exog=x_mat,
+              dates=stage_2_df.date_time,
+              order=(0, 1, 1))
+
+model_fit = model.fit(disp=0)   # disp=0 turns off debug information
+with open('model5.txt', 'w') as f:
+    # print summary
+    print >> f, model_fit.summary()
+
+
+# ARIMA MODEL #6: STAGE 2, STRICTLY STATISTICAL INTERACTIONS (REMOVING SOME INSIGNIF. VARS)
+# ALSO BUMPING UP TO 2 MA COEFFICIENTS..BOTH ARE STATISTICALLY SIGNIFICANT
+x_mat = stage_2_df >> select(stage_2_df.away_goal_AND_upset,
+                             stage_2_df.man_down_AND_upset,
+                             stage_2_df.competitive_idx)
+
+model = ARIMA(endog=stage_2_df.shorthand_search_vol,
+              exog=x_mat,
+              dates=stage_2_df.date_time,
+              order=(0, 1, 2))
+
+model_fit = model.fit(disp=0)   # disp=0 turns off debug information
+with open('model6.txt', 'w') as f:
+    # print summary
+    print >> f, model_fit.summary()
--- Analysis/Modeling/plots/plotting.py
+++ Analysis/Modeling/plots/plotting.py
@@ -36,12 +36,11 @@ p = ggplot(aes(x='date_time',
                y='shorthand_search_vol',
                group="match_id",
                color="match_id"),
-           data=longform_df >> sift(X.match_id == "chelsea2015-08-16"))
-
+           data=longform_df >> sift((X.match_id == "chelsea2015-08-16") |
+                                    (X.match_id == "manchester_city2015-08-16")))
 p += geom_line()
-
-p += labs(x="Time (GMT)", y="Search Volume (Scaled to 100)")
-p += ggtitle("Manchester City (H) vs. Chelsea (A), August 8th 2016, Etihad Stadium")
+p += labs(x="time (gmt)", y="search volume (scaled to 100)")
+p += ggtitle("man. city (h) vs. chelsea (a), aug. 8 '16, etihad stadium")
 
 # add in shaded rectangle to indicate actual match TODO
 # rect_df = longform_df >> sift(X.stage_2_ind == 1) >> \
@@ -57,4 +56,38 @@ p += ggtitle("Manchester City (H) vs. Chelsea (A), August 8th 2016, Etihad Stadi
 #     p += geom_vline(xintercept=[row["date_time"]], color='red')
 
 p += scale_x_date(labels=date_format("%H:%M:%S"), date_breaks="30 minutes")
-p.save('chelsea2015-08-16.png', width=25, height=10)
+p.save('chelsea_manchester_city2015-08-16.png', width=25, height=10)
+
+# use the below to inform assumptions about ARIMA parameters
+
+# let's only plot stage 2 of that match
+stage_2_df = longform_df >> sift(X.stage_2_ind == 1) \
+                         >> sift((X.match_id == "chelsea2015-08-16") |
+                                 (X.match_id == "manchester_city2015-08-16"))
+
+p = ggplot(aes(x='date_time',
+               y='shorthand_search_vol',
+               group="match_id",
+               color="match_id"),
+           data=stage_2_df)
+
+p += geom_line()
+p += scale_x_date(labels=date_format("%H:%M:%S"), date_breaks="1 hour")
+p += labs(x="time (gmt)", y="search volume (scaled to 100)")
+p += ggtitle("[match only] man. city (h) vs. chelsea (a), aug. 8 '16, etihad stadium")
+p.save('chelsea_manchester_city2015-08-16_stage_2.png', width=25, height=10)
+
+# and now let's plot the "stationarized" version (first difference)
+stage_2_df["first_diff"] = stage_2_df.shorthand_search_vol.diff(periods=1)
+p = ggplot(aes(x='date_time',
+               y='first_diff',
+               group="match_id",
+               color="match_id"),
+           data=stage_2_df)
+
+p += geom_line()
+p += scale_x_date(labels=date_format("%H:%M:%S"), date_breaks="1 hour")
+p += labs(x="time (gmt)", y="+/- differences in search volume (per 8 minutes)")
+p += geom_hline(y=0, color='BurlyWood')
+p += ggtitle("[Stationarized] man. city (h) vs. chelsea (a), aug. 8 '16, etihad stadium")
+p.save('chelsea_manchester_city2015-08-16_stationarized.png', width=25, height=10)
