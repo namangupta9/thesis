9d24ff52ac843c532258bcab8c2da60e0b2d35d6
namang <namangupta9@gmail.com>
2018-03-28 19:21:27 -0400

    dkf

--- Analysis/Modeling/multiple regression/plotting_helper.py
+++ Analysis/Modeling/multiple regression/plotting_helper.py
@@ -4,19 +4,44 @@ import pandas as pd
 import matplotlib
 matplotlib.use('TkAgg')
 from ggplot import *
-from dplython import *
 
 
-def plot_match(df, x_var, y_var, match_id, filename_out):
-    """Plot and save based on specified variables."""
-    pass
+def plot_matches(df, filename_out, x_var='date_time',
+                 y_var="shorthand_search_vol"):
+    """
+    Plot y-var and save based on specified variables.
+
+    Assumes that df has already been filtered using dplyr's sift mechanism.
+    """
+    # basic data processing for viz
+    df["date"] = df.match_id.apply(lambda x: "20" + x.split("20")[-1])
+    df['date_time'] = df['date'].astype(str) + " " + df['time'].astype(str)
+    df['date_time'] = pd.to_datetime(df['date_time'],
+                                     errors="coerce",
+                                     infer_datetime_format=True)
+
+    # build layers for plot
+    p = ggplot(aes(x=x_var,
+                   y=y_var,
+                   group="match_id",
+                   color="match_id"), data=df)
+    p += geom_line()
+
+    # informative
+    p += labs(x="time (gmt)", y="search volume (scaled to 100)")
+    # p += ggtitle("man. city (h) vs. chelsea (a)\naug. 8 '16, etihad stadium")
+    p += scale_x_date(labels=date_format("%H:%M:%S"), date_breaks="30 minutes")
+
+    # visual
+    t = theme_gray()
+    t._rcParams['font.size'] = 8
+    t._rcParams['font.family'] = 'monospace'
+    p += t
+
+    # done
+    p.save(filename_out, width=16, height=8)
 
 
 def plot_match_and_preds(df, x_var, y_var, match_id, filename_out):
     """Plot y as well as y-hat (actual vs. prediction) for a given match."""
-    pass
-
-
-def plot_match_range():
-    """Plot multiple matches at once."""
     pass
