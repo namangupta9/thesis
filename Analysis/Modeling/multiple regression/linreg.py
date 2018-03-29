9fa2dd7fd15010172192634873d88e90f169abd8
namang <namangupta9@gmail.com>
2018-03-29 00:56:23 -0400

    predictive plots for linreg

--- Analysis/Modeling/multiple regression/linreg.py
+++ Analysis/Modeling/multiple regression/linreg.py
@@ -5,104 +5,125 @@ from dplython import DplyFrame, sift, X
 import statsmodels.api as sm
 from plotting_helper import plot_matches
 
+
+def predict_on_match_id(lm, longform_df, date, match_id, opponent_match_id,
+                        model_no, filename_out, x_var_list):
+    """Run prediction & plot / save on top of match search vol."""
+    match_data = longform_df >> sift(X.match_id == match_id)
+    candidate_data = match_data[x_var_list]
+    preds = [(model_no, pred) for pred in lm.predict(candidate_data)]
+
+    # turn predictions into data frame
+    labels = ["match_id", "shorthand_search_vol"]
+    temp_df = pd.DataFrame.from_records(preds, columns=labels)
+    temp_df = temp_df.set_index(match_data.index)
+    temp_df["time"] = match_data["time"]
+
+    # append to long form df and plot
+    preds_df = pd.concat([longform_df, temp_df], axis=0, ignore_index=True)
+    sifted_df = preds_df >> sift((X.match_id == match_id) |
+                                 (X.match_id == opponent_match_id) |
+                                 (X.match_id == model_no))
+
+    plot_matches(sifted_df, date, filename_out)
+
+
 # read & dissect df
-longform_df = DplyFrame(pd.read_csv("../../LongForm/longform.csv",
-                        dtype={'shorthand_search_vol': int}))
+longform_df_og = pd.read_csv("../../LongForm/longform.csv",
+                             dtype={'shorthand_search_vol': int})
+longform_df = DplyFrame(longform_df_og)
 cols = longform_df.columns
 y_var = longform_df["shorthand_search_vol"]       # search volume
 longform_df['ones'] = 1
-
-# match_df = longform_df >> sift(X.stage_2_ind == 1) \
-#                        >> sift((X.match_id == "chelsea2015-08-16") |
-#                                (X.match_id == "manchester_city2015-08-16"))
-
-# TODO .predict(), and then plot the predicted values vs. actual values
-# create a new match_id for predicted values
-# and concatenate predicted values, match_id to existing data frame
-# then pass into plot_matches(), and it will plot another line easily
-
-# also have a column for the residual
 
 
 # MLR MODEL #1: USING ONLY MATCH STAGES TO PREDICT SEARCH VOLUME
 # note the difference in argument order; y_var is dependent, x_vars independent
 # using Stage 0 as "reference level"; only vars are stage_1-4_indicators
-x_vars = longform_df[["ones",
-                      "stage_1_ind",
-                      "stage_2_ind",
-                      "stage_3_ind",
-                      "stage_4_ind"]]
+x_var_list = ["ones", "stage_1_ind", "stage_2_ind",
+              "stage_3_ind", "stage_4_ind"]
+x_vars = longform_df[x_var_list]
 lm = sm.OLS(y_var, x_vars).fit()
 with open('model1.txt', 'w') as f:
     print >> f, lm.summary()
-plot_matches()  # TODO
+predict_on_match_id(lm=lm,
+                    longform_df=longform_df,
+                    date="2015-08-16",
+                    match_id="chelsea2015-08-16",
+                    opponent_match_id="manchester_city2015-08-16",
+                    model_no="linreg_model1",
+                    filename_out="model1.png",
+                    x_var_list=x_var_list)
+
 
 # MLR MODEL #2: USING MATCH STAGES + COMPETITIVE INDEX TO PREDICT SEARCH VOL
-x_vars = longform_df[["ones",
-                      "stage_1_ind",
-                      "stage_2_ind",
-                      "stage_3_ind",
-                      "stage_4_ind",
-                      "competitive_idx"]]
+x_var_list += ["competitive_idx"]
+x_vars = longform_df[x_var_list]
 lm = sm.OLS(y_var, x_vars).fit()
 with open('model2.txt', 'w') as f:
     print >> f, lm.summary()
+predict_on_match_id(lm=lm,
+                    longform_df=longform_df,
+                    date="2015-08-16",
+                    match_id="chelsea2015-08-16",
+                    opponent_match_id="manchester_city2015-08-16",
+                    model_no="linreg_model2",
+                    filename_out="model2.png",
+                    x_var_list=x_var_list)
 
 
 # MLR MODEL #3: USING STAGES + COMPETITIVE INDEX + EVENTS TO PREDICT SEARCH VOL
-x_vars = longform_df[["ones",
-                      "stage_1_ind",
-                      "stage_2_ind",
-                      "stage_3_ind",
-                      "stage_4_ind",
-                      "competitive_idx",
-                      "home_goal",
-                      "away_goal",
-                      "home_yellow",
-                      "away_yellow",
-                      "home_red",
-                      "away_red"]]
+x_var_list += ["home_goal", "away_goal", "home_yellow", "away_yellow",
+               "home_red", "away_red"]
+x_vars = longform_df[x_var_list]
 lm = sm.OLS(y_var, x_vars).fit()
 with open('model3.txt', 'w') as f:
     print >> f, lm.summary()
+predict_on_match_id(lm=lm,
+                    longform_df=longform_df,
+                    date="2015-08-16",
+                    match_id="chelsea2015-08-16",
+                    opponent_match_id="manchester_city2015-08-16",
+                    model_no="linreg_model3",
+                    filename_out="model3.png",
+                    x_var_list=x_var_list)
 
 
 # MLR MODEL #4: USING STAGES + COMPETITIVE INDEX + MATCH STATE
-x_vars = longform_df[["ones",
-                      "stage_1_ind",
-                      "stage_2_ind",
-                      "stage_3_ind",
-                      "stage_4_ind",
-                      "competitive_idx",
-                      "cum_total_goals",
-                      "cum_goal_diff",
-                      "man_down",
-                      "upset"]]
+x_var_list = ["ones", "stage_1_ind", "stage_2_ind",
+              "stage_3_ind", "stage_4_ind", "competitive_idx",
+              "cum_total_goals", "cum_goal_diff", "man_down", "upset"]
+x_vars = longform_df[x_var_list]
 lm = sm.OLS(y_var, x_vars).fit()
 with open('model4.txt', 'w') as f:
     print >> f, lm.summary()
+predict_on_match_id(lm=lm,
+                    longform_df=longform_df,
+                    date="2015-08-16",
+                    match_id="chelsea2015-08-16",
+                    opponent_match_id="manchester_city2015-08-16",
+                    model_no="linreg_model4",
+                    filename_out="model4.png",
+                    x_var_list=x_var_list)
 
 
 # MLR MODEL #5: USING ALL VARS
-x_vars = longform_df[["ones",
-                      "stage_1_ind",
-                      "stage_2_ind",
-                      "stage_3_ind",
-                      "stage_4_ind",
-                      "competitive_idx",
-                      "home_goal",
-                      "away_goal",
-                      "home_yellow",
-                      "away_yellow",
-                      "home_red",
-                      "away_red",
-                      "cum_total_goals",
-                      "cum_goal_diff",
-                      "man_down",
-                      "upset"]]
+x_var_list = ["ones", "stage_1_ind", "stage_2_ind", "stage_3_ind",
+              "stage_4_ind", "competitive_idx", "home_goal", "away_goal",
+              "home_yellow", "away_yellow", "home_red", "away_red",
+              "cum_total_goals", "cum_goal_diff", "man_down", "upset"]
+x_vars = longform_df[x_var_list]
 lm = sm.OLS(y_var, x_vars).fit()
 with open('model5.txt', 'w') as f:
     print >> f, lm.summary()
+predict_on_match_id(lm=lm,
+                    longform_df=longform_df,
+                    date="2015-08-16",
+                    match_id="chelsea2015-08-16",
+                    opponent_match_id="manchester_city2015-08-16",
+                    model_no="linreg_model5",
+                    filename_out="model5.png",
+                    x_var_list=x_var_list)
 
 
 # BUILDING IN STATISTICAL INTERACTIONS
@@ -116,94 +137,53 @@ longform_df['home_goal_AND_deadlock'] = longform_df['home_goal'].astype(int) * l
 longform_df['away_goal_AND_deadlock'] = longform_df['away_goal'].astype(int) * longform_df['deadlock'].astype(int)
 
 # MLR MODEL #6: USING INTERACTIONS
-x_vars = longform_df[["ones",
-                      "stage_1_ind",
-                      "stage_2_ind",
-                      "stage_3_ind",
-                      "stage_4_ind",
-                      "competitive_idx",
-                      "home_goal",
-                      "away_goal",
-                      "home_yellow",
-                      "away_yellow",
-                      "home_red",
-                      "away_red",
-                      "man_down",
-                      "upset",
-                      "home_goal_AND_upset",
-                      "away_goal_AND_upset",
-                      "home_goal_AND_man_down",
-                      "away_goal_AND_man_down",
-                      "man_down_AND_upset",
-                      "home_goal_AND_deadlock",
-                      "away_goal_AND_deadlock"]]
+x_var_list = ["ones", "stage_1_ind", "stage_2_ind", "stage_3_ind",
+              "stage_4_ind", "competitive_idx", "home_goal", "away_goal",
+              "home_yellow", "away_yellow", "home_red", "away_red",
+              "cum_total_goals", "cum_goal_diff", "man_down", "upset",
+              "home_goal_AND_upset", "away_goal_AND_upset",
+              "home_goal_AND_man_down", "away_goal_AND_man_down",
+              "man_down_AND_upset", "home_goal_AND_deadlock",
+              "away_goal_AND_deadlock"]
+x_vars = longform_df[x_var_list]
 lm = sm.OLS(y_var, x_vars).fit()
 with open('model6.txt', 'w') as f:
     print >> f, lm.summary()
+predict_on_match_id(lm=lm,
+                    longform_df=longform_df,
+                    date="2015-08-16",
+                    match_id="chelsea2015-08-16",
+                    opponent_match_id="manchester_city2015-08-16",
+                    model_no="linreg_model6",
+                    filename_out="model6.png",
+                    x_var_list=x_var_list)
 
 
 # MLR MODEL #7: USING INTERACTIONS, ONLY STAGE 2
 stage_2_df = longform_df >> sift(X.stage_2_ind == 1)
 stage_2_df = stage_2_df.reset_index(drop=True)
 y_var = stage_2_df["shorthand_search_vol"]
-x_vars = stage_2_df[["ones",
-                     "competitive_idx",
-                     "home_goal",
-                     "away_goal",
-                     "home_yellow",
-                     "away_yellow",
-                     "home_red",
-                     "away_red",
-                     "man_down",
-                     "upset",
-                     "home_goal_AND_upset",
-                     "away_goal_AND_upset",
-                     "home_goal_AND_man_down",
-                     "away_goal_AND_man_down",
-                     "man_down_AND_upset",
-                     "home_goal_AND_deadlock",
-                     "away_goal_AND_deadlock"]]
+x_var_list = ["ones", "competitive_idx", "home_goal", "away_goal",
+              "home_yellow", "away_yellow", "home_red", "away_red",
+              "cum_total_goals", "cum_goal_diff", "man_down", "upset",
+              "home_goal_AND_upset", "away_goal_AND_upset",
+              "home_goal_AND_man_down", "away_goal_AND_man_down",
+              "man_down_AND_upset", "home_goal_AND_deadlock",
+              "away_goal_AND_deadlock"]
+x_vars = stage_2_df[x_var_list]
 lm = sm.OLS(y_var, x_vars).fit()
 with open('model7.txt', 'w') as f:
     print >> f, lm.summary()
-
-# MLR MODEL #8: USING INTERACTIONS, ONLY STAGE 2
-stage_2_df = longform_df >> sift(X.stage_2_ind == 1)
-stage_2_df = stage_2_df.reset_index(drop=True)
-y_var = stage_2_df["shorthand_search_vol"]
-x_vars = stage_2_df[["ones",
-                     "stage_2_ind",
-                     "competitive_idx",
-                     "home_goal",
-                     "away_goal",
-                     "home_yellow",
-                     "away_yellow",
-                     "home_red",
-                     "away_red",
-                     "man_down",
-                     "upset",
-                     "home_goal_AND_upset",
-                     "away_goal_AND_upset",
-                     "home_goal_AND_man_down",
-                     "away_goal_AND_man_down",
-                     "man_down_AND_upset",
-                     "home_goal_AND_deadlock",
-                     "away_goal_AND_deadlock"]]
-lm = sm.OLS(y_var, x_vars).fit()
-with open('model8.txt', 'w') as f:
-    print >> f, lm.summary()
-
+predict_on_match_id(lm=lm,
+                    longform_df=stage_2_df,
+                    date="2015-08-16",
+                    match_id="chelsea2015-08-16",
+                    opponent_match_id="manchester_city2015-08-16",
+                    model_no="linreg_model7",
+                    filename_out="model7.png",
+                    x_var_list=x_var_list)
 
 # OTHER COMMENTS Schwartz Left
-
-# print('Parameters: ', lm.params)
-# print('R2: ', lm.rsquared)
-
-# # make the predictions by the model
-# predictions = lm.predict(x_vars)
-# # this should give predictions, once we have the const stuff figured out.
-# # this was the goal time for chel / bournemouth
-# print predictions.iloc[110:120]
 
 # also make an error column; y_var - prediciton; the "residual"
 # for what games do you find a high level of error?
--- Analysis/Modeling/multiple regression/plotting_helper.py
+++ Analysis/Modeling/multiple regression/plotting_helper.py
@@ -7,25 +7,25 @@ from ggplot import (ggplot, geom_line, labs, scale_x_date,
                     theme_gray, aes, date_format)
 
 
-def plot_matches(df, filename_out, x_var='date_time',
+def plot_matches(df_in, date, filename_out, x_var='date_time',
                  y_var="shorthand_search_vol"):
     """
     Plot y-var and save based on specified variables.
 
     Assumes that df has already been filtered using dplyr's sift mechanism.
+    Also assumes that a date has been passed in.
     """
     # basic data processing for viz
-    df["date"] = df.match_id.apply(lambda x: "20" + x.split("20")[-1])
-    df['date_time'] = df['date'].astype(str) + " " + df['time'].astype(str)
-    df['date_time'] = pd.to_datetime(df['date_time'],
-                                     errors="coerce",
-                                     infer_datetime_format=True)
+    df_in['date_time'] = date + " " + df_in['time'].astype(str)
+    df_in['date_time'] = pd.to_datetime(df_in['date_time'],
+                                        errors="coerce",
+                                        infer_datetime_format=True)
 
     # build layers for plot
     p = ggplot(aes(x=x_var,
                    y=y_var,
                    group="match_id",
-                   color="match_id"), data=df)
+                   color="match_id"), data=df_in)
     p += geom_line()
 
     # informative
