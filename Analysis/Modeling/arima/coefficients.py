9ebbff10242b0dd765bea337e94884b0ef81ca14
namang <namangupta9@gmail.com>
2018-04-13 15:42:29 -0400

    dlfkj

--- Analysis/Modeling/arima/coefficients.py
+++ Analysis/Modeling/arima/coefficients.py
@@ -6,11 +6,11 @@ from dplython import DplyFrame
 import matplotlib
 matplotlib.use('TkAgg')
 from ggplot import (ggplot, geom_density, scale_color_brewer,
-                    scale_x_continuous,
-                    theme_bw, aes) # noqa: E402
+                    scale_x_continuous, facet_wrap, labs, ggtitle,
+                    theme_gray, aes) # noqa: E402
 
 
-# data frame processing
+# data frame processing; creating a master df for faceting plots
 json_filename = "arima_1.json"
 coefficient_dict = json.load(open(json_filename))
 coefficients = DplyFrame(pd.DataFrame.from_dict(coefficient_dict,
@@ -18,29 +18,35 @@ coefficients = DplyFrame(pd.DataFrame.from_dict(coefficient_dict,
 coefficients = coefficients.transpose()
 folder_name = json_filename.split('.')[0]
 
+dfs_to_concat = []
 for feature in ["home_goal", "away_goal", "home_yellow", "away_yellow",
-                "home_red", "home_yellow"]:
+                "home_red", "away_red"]:
 
-    # create a new dataframe for clean plotting purposes
+    # create a new long-form dataframe for clean plotting purposes
     values_dict = {"significant": coefficients[feature]["significant"],
-                   "unsignificant": coefficients[feature]["unsignificant"]}
-
+                   "insignificant": coefficients[feature]["unsignificant"]}
     df = pd.DataFrame.from_dict(values_dict, orient='index')
     df = df.transpose()
-    df = pd.melt(df)    # convert to long-form
-    # print(df)
+    df = pd.melt(df)
+    df['feature'] = feature
+    dfs_to_concat.append(df)
 
-    # data
-    p = ggplot(aes(x='value', color='variable'), data=df)
-    p += geom_density(fill=True)
-    p += scale_x_continuous(limits=(-25, 25))
+master_df = pd.concat(dfs_to_concat)
 
-    # visual
-    t = theme_bw()
-    t._rcParams['font.size'] = 10
-    t._rcParams['font.family'] = 'monospace'
-    p += t
+# data
+p = ggplot(aes(x='value', color='variable'), data=master_df)
+p += geom_density(fill=True)
+p += scale_x_continuous(limits=(-25, 25))
+p += scale_color_brewer(type='qual', palette=3)
+p += ggtitle("sarimax coefficient magnitude distributions")
+p += facet_wrap("feature", ncol=3)
 
-    p.save("arima_1/" + feature + ".png")
+# visual
+t = theme_gray()
+t._rcParams['font.size'] = 10
+t._rcParams['font.family'] = 'monospace'
+p += t
 
-    # TODO how to create faceted plot of each feature
+p.save("arima_1/" + "faceted.png")
+
+# TODO how to create faceted plot of each feature
